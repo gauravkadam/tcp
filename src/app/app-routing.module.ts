7d5db9d05c885f9db65210b6e626dcd3a0356f2a
Gaurav Kadam <gauravkadam123@gmail.com>
2020-11-06 22:32:04 +0530

    Updated angular version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'my-courses',
+    loadChildren: () => import('../app/modules/my-courses/my-courses.module').then(m => m.MyCoursesModule)
+  },
+  {
+    path: 'forum',
+    loadChildren: () => import('../app/modules/forum/forum.module').then(m => m.ForumModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
