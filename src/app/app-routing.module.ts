8ca08a99681279eff150322415b50d796156f18c
Gaurav Kadam <gauravkadam123@gmail.com>
2020-11-07 21:30:39 +0530

    added feature modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,28 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'course-catalog',
+    loadChildren: () => import('../app/modules/course-catalog/course-catalog.module').then(m => m.CourseCatalogModule)
+  },
+  {
     path: 'my-courses',
     loadChildren: () => import('./modules/my-courses/my-courses.module').then(m => m.MyCoursesModule)
   },
   {
     path: 'forum',
     loadChildren: () => import('../app/modules/forum/forum.module').then(m => m.ForumModule)
+  },
+  {
+    path: 'my-students',
+    loadChildren: () => import('../app/modules/my-students/my-students.module').then(m => m.MyStudentsModule)
+  },
+  {
+    path: 'videos',
+    loadChildren: () => import('../app/modules/videos/videos.module').then(m => m.VideosModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('../app/components/login/login.module').then(m => m.LoginModule)
   },
   {
     path: '',
--- src/app/modules/my-students/my-students.module.ts
+++ src/app/modules/my-students/my-students.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MyStudentsComponent } from './my-students.component';
+import { Routes, RouterModule } from '@angular/router';
 
+
+const routes: Routes = [
+  { path: '', component: MyStudentsComponent }
+]
 
 
 @NgModule({
   declarations: [MyStudentsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class MyStudentsModule { }
