2cf663c93c98f5e182e49639404413cf9e7333c5
Gaurav Kadam <gauravkadam123@gmail.com>
2020-11-26 18:56:14 +0530

    add course changes

--- src/app/modules/my-courses/add-course/add-course.component.ts
+++ src/app/modules/my-courses/add-course/add-course.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-course',
-  templateUrl: './add-course.component.html',
-  styleUrls: ['./add-course.component.scss']
+   selector: 'app-add-course',
+   templateUrl: './add-course.component.html',
+   styleUrls: ['./add-course.component.scss']
 })
 export class AddCourseComponent implements OnInit {
 
-  constructor() { }
+   courseNames = ['Web-developement', 'Data Analyst', 'Machine Learning', 'Artificial Intelligence'];
+   courseLevel = ['Beginner', 'Intermediate', 'Advanced'];
+   currentYear: number;
+   yearsList = [];
+   constructor() { }
 
-  ngOnInit(): void {
-  }
+   ngOnInit(): void {
+      const years = new Date().getFullYear();
+      this.getYears(years);
+   }
+
+   getYears(year: number) {
+      for (let i = 0; i  < 10; i++) {
+         this.yearsList.push(year);
+         year++;
+      }
+   }
 
 }
