e00711d31efec8b610159abbd9575b2e8c02dc88
Gaurav Kadam <gauravkadam123@gmail.com>
2020-11-28 23:25:24 +0530

    ui changes for add task

--- src/app/modules/my-courses/add-task/add-task.component.ts
+++ src/app/modules/my-courses/add-task/add-task.component.ts
@@ -9,7 +9,35 @@ export class AddTaskComponent implements OnInit {
 
   constructor() { }
 
+  public imagePath;
+  public imgURL: any;
+  public message: string;
+public fileElement: any;
+ 
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
+
   ngOnInit(): void {
+    this.fileElement = document.getElementById('upload-file');
+  }
+
+  uploadFile(){
+    this.fileElement.click();
   }
 
 }
